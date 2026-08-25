c90d0047b7ea5bbcb2c011f813e33a80a316e10f
hamahasan441-png <hamahasan441@gmail.com>
2026-08-25 15:01:58 +0200

    fix(chart): import indicator panel row weight

--- app/src/main/java/com/foxtrader/app/feature/chart/presentation/components/IndicatorPanel.kt
+++ app/src/main/java/com/foxtrader/app/feature/chart/presentation/components/IndicatorPanel.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
